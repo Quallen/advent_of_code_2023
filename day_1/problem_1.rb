eb76bf7ecffa1167eb32392fe439cd6c643c8511
Quallen <alexander.holland@ctunitedway.org>
2023-12-07 18:14:23 -0500

    oh i hate them I can't clean the line with gsub replacement eg a line that ends with oneight needs to be 8 not 1

--- day_1/problem_1.rb
+++ day_1/problem_1.rb
@@ -15,7 +15,19 @@ class Calibration
     "#{first_digit}#{second_digit}".to_i
   end
 
+  def replace_pseudo_values(line: )
+    substitutions = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}.with_indifferent_access
+    regex = Regexp.union(substitutions.keys)
+    converted_line = line.gsub(regex, substitutions)
+    calibration_value(line: converted_line)
+  end
+
   def calculate
     calibration_array.map{|line| calibration_value(line: line) }.reduce(:+)
+  end
+
+  # spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+  def calculate_using_pseudo_digits
+    calibration_array.map{|line| replace_pseudo_values(line: line) }.reduce(:+)
   end
 end
