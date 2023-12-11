3c1ec29e2c054b5dfc5f20b706ffb922091ad014
Quallen <alexander.holland@ctunitedway.org>
2023-12-10 20:02:18 -0500

    add card hash with card id as key and array of cards as value. walk cards and for each copy in cards_hash create new cards and and to cards_hash. Sum total cards

--- day_4/day_4.rb
+++ day_4/day_4.rb
@@ -2,11 +2,12 @@ require 'active_support/all'
 
 class Scratchcards
 
-  attr_accessor :scratchcards, :cards
+  attr_accessor :scratchcards, :cards, :cards_hash
 
   def initialize
     @scratchcards = File.read('day_4_input.txt').lines.map(&:chomp)
     @cards = []
+    @cards_hash = {}
     initialize_cards
   end
 
@@ -20,18 +21,40 @@ class Scratchcards
       numbers = all_numbers.last.split(' ').map(&:to_i)
       cards << Card.new(id: id, winning_numbers: winning_numbers, numbers: numbers)
     end
+
+    cards.each do |card|
+      cards_hash[card.id] = Array.new
+      cards_hash[card.id] << MemoCard.new(id: card.id, number_of_matches: card.matches)
+    end
   end
 
   def total_points
     cards.map{|card| card.value}.reduce(:+)
   end
 
+  def run_game
+    cards.each do |card|
+      all_copies = cards_hash[card.id]
+      id = card.id
+      all_copies.each do |a_card|
+        a_card.number_of_matches.times do |offset|
+          new_id = id + 1 + offset
+          new_card = cards_hash[new_id].first
+          next if new_card.nil?
+          cards_hash[new_id] << MemoCard.new(id: new_card.id, number_of_matches: new_card.number_of_matches)
+        end
+      end
+    end
+    cards.map{|card| cards_hash[card.id].count}.reduce(:+)
+  end
+
   class Card
-    attr_accessor :id, :winning_numbers, :numbers
+    attr_accessor :id, :winning_numbers, :numbers, :matches
     def initialize(id:, winning_numbers:, numbers: )
       @id = id
       @winning_numbers = winning_numbers
       @numbers = numbers
+      @matches ||= number_of_matches
     end
 
     def number_of_matches
@@ -49,4 +72,14 @@ class Scratchcards
       end
     end
   end
+
+  class MemoCard
+    attr_accessor :id, :number_of_matches
+    def initialize(id:, number_of_matches: )
+      @id = id
+      @number_of_matches = number_of_matches
+    end
+  end
 end
+
+puts Scratchcards.new.run_game
