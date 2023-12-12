6afb54af34315957b0d9c3a61bd929baa6b7ddd8
Quallen <alexander.holland@ctunitedway.org>
2023-12-12 18:26:21 -0500

    CamelCards, Hand, Card. CamelCards has a list of hands. Hands have a list of cards. Hands are sorted into buckets by stregnth of type of match and then each bucket is sorted by strength of cards

---
+++ day_7/day_7.rb
@@ -0,0 +1,169 @@
+require 'active_support/all'
+require 'pry-byebug'
+
+class CamelCards
+  attr_accessor :input, :hands, :sorted_hands, :bucket_keys
+
+  def initialize
+    @input = File.read('day_7_input.txt').lines.map(&:chomp)
+    @hands = []
+    @bucket_keys = [:five_of_a_kind, :four_of_a_kind, :full_house, :three_of_a_kind, :two_pair, :one_pair, :high_card]
+    get_hands
+    @sorted_hands = Hash.new
+    @current_rank = 1
+    put_hands_in_buckets
+    sort_buckets
+    rank_hands
+  end
+
+  def score_game
+    puts hands.map{|hand| hand.rank * hand.bid}.reduce(:+)
+  end
+
+  def get_hands
+    input.each do |line|
+      hands << Hand.new(cards_string: line.split(' ').first, bid: line.split(' ').last)
+    end
+  end
+
+  def sort_buckets
+    bucket_keys.each do |key|
+      sorted = sorted_hands[key].sort_by{|hand| [hand.cards.first.strength, hand.cards.second.strength , hand.cards.third.strength, hand.cards.fourth.strength, hand.cards.last.strength]}
+      sorted_hands[key] = sorted
+    end
+  end
+
+  def rank_hands
+    bucket_keys.reverse.each do |key|
+      hands = sorted_hands[key]
+      hands.each do |hand|
+        this_rank = @current_rank
+        hand.rank = this_rank
+        @current_rank += 1
+      end
+    end
+  end
+
+  def put_hands_in_buckets
+    bucket_keys.each do |key|
+      sorted_hands[key] = []
+    end
+
+    hands.each do |hand|
+      if hand.five_of_a_kind?
+        sorted_hands[:five_of_a_kind] << hand
+      elsif hand.four_of_a_kind?
+        sorted_hands[:four_of_a_kind] << hand
+      elsif hand.full_house?
+        sorted_hands[:full_house] << hand
+      elsif hand.three_of_a_kind?
+        sorted_hands[:three_of_a_kind] << hand
+      elsif hand.two_pair?
+        sorted_hands[:two_pair] << hand
+      elsif hand.one_pair?
+        sorted_hands[:one_pair] << hand
+      elsif hand.high_card?
+        sorted_hands[:high_card] << hand
+      end
+    end
+  end
+
+  class Hand
+    attr_accessor :cards, :card_strengths, :card_values, :rank, :bid
+    def initialize(cards_string: , bid:)
+      @card_values = %w(A K Q J T 9 8 7 6 5 4 3 2)
+      @card_strengths = card_strengths_hash
+      @cards = build_cards(cards_string: cards_string)
+      @bid = bid.to_i
+      @rank = 0
+    end
+
+    def build_cards(cards_string:)
+      cards_list = []
+      cards_string.chars.each do |card|
+        cards_list << Card.new(card_face: card, card_strength: card_strengths[card])
+      end
+      cards_list
+    end
+
+    def card_strengths_hash
+      hash = {}
+      card_values.reverse.each_with_index do |card,index|
+        hash[card] = index
+      end
+      hash
+    end
+
+    def five_of_a_kind?
+      card_values.each do |value|
+        return true if cards.all?{|card| card.text == value}
+      end
+      false
+    end
+
+    def four_of_a_kind?
+      card_values.each do |value|
+        return true if cards.select{|card| card.text == value}.count == 4
+      end
+      false
+    end
+
+    def full_house?
+      three_cards = nil
+      two_cards = nil
+      card_values.each do |value|
+        three_cards = value if cards.select{|card| card.text == value}.count == 3
+        two_cards = value if cards.select{|card| card.text == value}.count == 2
+        return true if three_cards && two_cards
+      end
+      false
+    end
+
+    def three_of_a_kind?
+      card_values.each do |value|
+        return true if cards.select{|card| card.text == value}.count == 3
+      end
+      false
+    end
+
+    def two_pair?
+      first_pair = nil
+      second_pair = nil
+      card_values.each do |value|
+        first_pair = value if cards.select{|card| card.text == value}.count == 2
+        if first_pair
+          card_values.each do |c_val|
+            second_pair = c_val if ( cards.reject{|card| card.text == first_pair}.select{|card| card.text == c_val}.count == 2 )
+          end
+        end
+
+        return true if first_pair && second_pair
+      end
+      false
+    end
+
+    def one_pair?
+      card_values.each do |value|
+        return true if cards.select{|card| card.text == value}.count == 2
+      end
+      false
+    end
+
+    def high_card?
+      card_values.each do |value|
+        return true if cards.select{|card| card.text == value}.count == 1
+      end
+      false
+    end
+  end
+
+  class Card
+    attr_accessor :input, :text, :strength
+    def initialize(card_face: , card_strength:)
+      @text = card_face
+      @strength = card_strength
+    end
+  end
+end
+
+CamelCards.new.score_game
