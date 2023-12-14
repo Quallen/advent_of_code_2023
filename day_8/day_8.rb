f28c3bbbdf2b52cddd44cfb2667925248ae691e5
Quallen <alexander.holland@ctunitedway.org>
2023-12-13 22:53:17 -0500

    good luck solving this with a general solution, i kind of hate problems like this. you are just boned until you notice the ghosts are walking in loops. once you have confirmed they are looping you can calc the least common multiple

--- day_8/day_8.rb
+++ day_8/day_8.rb
@@ -1,13 +1,18 @@
 require 'active_support/all'
+require 'pry-byebug'
 
 class Network
-  attr_accessor :input, :instructions, :nodes, :current_position
+  attr_accessor :input, :instructions, :nodes, :current_position, :ghosts, :first_end_locations, :second_end_locations
 
   def initialize
     @input = File.read('day_8_input.txt').lines.map(&:chomp)
     @instructions = input.first
     @nodes = build_nodes
     @current_position = nodes.select{|node| node.start_location? }.first
+    @ghosts =  nodes.select{|node| node.start_location? }
+    @first_end_locations = {}
+    @second_end_locations = {}
+    @steps = 0
   end
 
   def build_nodes
@@ -22,6 +27,22 @@ class Network
 
   def turn(move: )
     nodes.select{|node| node.location == move}.first
+  end
+
+  def ghost_walk
+    until second_end_locations.keys.count == 6 && second_end_locations.keys.all?{ |location| (@second_end_locations[location] % @first_end_locations[location]) == 0}
+      instructions.each_char do |char|
+        new_nodes = @ghosts.map{|node| char == 'L' ? turn(move: node.left) : turn(move: node.right) }
+        @steps += 1
+        @ghosts = new_nodes
+        new_nodes.each do |node|
+          @second_end_locations[node] = @steps if node.end_location? && @first_end_locations.keys.include?(node) && !@second_end_locations.keys.include?(node)
+          @first_end_locations[node] = @steps if node.end_location? && !@first_end_locations.keys.include?(node)
+        end
+        break if @second_end_locations.keys.count == 6 && second_end_locations.keys.all?{ |location| (@second_end_locations[location] % @first_end_locations[location]) == 0}
+      end
+    end
+    puts @first_end_locations.values.reduce(1, :lcm)
   end
 
   def steps_to_exit
@@ -47,13 +68,13 @@ class Network
     end
 
     def start_location?
-      location == 'AAA'
+      location.last == 'A'
     end
 
     def end_location?
-      location == 'ZZZ'
+      location.last == 'Z'
     end
   end
 end
 
-puts Network.new.steps_to_exit
+Network.new.ghost_walk
